21ef5b1fe30f60b5ca667eda87a69dc3847b095d
sk2589822 <sk25898222@gmail.com>
2021-03-06 17:08:23 +0800

    Feat: complete tasks
    1. 包含看懂題目和資料大概花了8小時
    2. 在CSS上卡滿久的，因為超過時間了，所以沒有完全照著設計稿做
    3. 因為時間已經超過了，所以背板樣式只有各取透明和普通的背板的第一個來做
    4. 沒有寫過BEM所以命名可能怪怪的
    5. 有些背板圖的url會是404，我有把iPhone 12 Pro max的圖換成其他還活著的圖

--- src/components/Purchase.vue
+++ src/components/Purchase.vue
@@ -7,11 +7,30 @@
     <div class="display__area">
       <section class="purchase__preview">
         <div class="purchase__preview-images-wrapper">
-          <img src="https://cdn.shopify.com/s/files/1/0740/2335/products/phone45-12-Pro_AppleGraphite_d04b461d-15c3-4e43-9b43-a828eaa4478c.png?v=1603352312" alt="" class="purchase__preview-image main">
-          <img src="https://cdn.shopify.com/s/files/1/0740/2335/products/Black-Bumper_87d2c086-7e6a-4f40-93d9-33828a249bbe.png?v=1597911746" alt="" class="purchase__preview-image sub case">
-          <img src="https://cdn.shopify.com/s/files/1/0740/2335/products/Black-Rim_2e73b29c-7262-463d-b047-2f91a790068a.png?v=1597911791" alt="" class="purchase__preview-image sub rim purchase__preview-image--backward">
-          <img src="https://cdn.shopify.com/s/files/1/0740/2335/products/Black-Button_23534b09-c1aa-4ab4-8174-ded2bc09069e.png?v=1597911773" alt="" class="purchase__preview-image sub button">
-          <img src="https://cdn.shopify.com/s/files/1/0740/2335/products/minisite-NPB0118526L_b4665252-c418-480f-9ca3-b84fb860ca2c.png?v=1603868182" alt="" class="purchase__preview-image sub backplate"></div>
+          <img :src="previewImageSrc.phone" alt="" class="purchase__preview-image main">
+          <img :src="previewImageSrc.frame" alt="" class="purchase__preview-image sub case">
+          <img
+            :src="previewImageSrc.rim"
+            alt=""
+            :class="[
+              'purchase__preview-image',
+              'sub',
+              'rim',
+              previewMode === 'rim' ? 'purchase__preview-image--forward' : 'purchase__preview-image--backward'
+            ]"
+          >
+          <img :src="previewImageSrc.button" alt="" class="purchase__preview-image sub button">
+          <img
+            :src="previewImageSrc.backPlate"
+            alt=""
+            :class="[
+              'purchase__preview-image',
+              'sub',
+              'backplate',
+              previewMode === 'backPlate' ? 'purchase__preview-image--forward' : 'purchase__preview-image--backward'
+            ]"
+          >
+          </div>
           <div class="purchase__selector-wrapper phone">
             <div class="color-container">
               <p class="color-picker-title">裝置<br>顏色</p>
@@ -36,11 +55,27 @@
               <section class="backplate-switch__common">
                 <div class="backplate-switch__option">
                   <div class="backplate-switch__checkbox backplate-switch__checkbox--active"></div>
-                  <p class="backplate-switch__mode-name">背板模式</p>
+                  <input
+                    v-model="previewMode"
+                    id="backPlate-radio"
+                    class="backplate-switch__option__radio"
+                    value="backPlate"
+                    type="radio"
+                    name="preview-mode"
+                  >
+                  <label class="backplate-switch__mode-name" for="backPlate-radio">背板模式</label>
                 </div>
                 <div class="backplate-switch__option">
                   <div class="backplate-switch__checkbox"></div>
-                  <p class="backplate-switch__mode-name">邊框模式</p>
+                  <input
+                    v-model="previewMode"
+                    id="rim-radio"
+                    class="backplate-switch__option__radio"
+                    value="rim"
+                    type="radio"
+                    name="preview-mode"
+                  >
+                  <label class="backplate-switch__mode-name" for="rim-radio">邊框模式</label>
                 </div>
               </section>
             </div>
@@ -51,7 +86,7 @@
     <div class="selection__area">
       <div class="selection__area__top">
         <div class="device__selector">
-          <select name="" id="device">
+          <select name="" id="device" @change="setProduct('init')" v-model="selectedDevice">
             <option value="iphone-12-pro-max">iPhone 12 Pro Max</option>
             <option value="iphone-12-pro">iPhone 12 Pro</option>
             <option value="iphone-12">iPhone 12</option>
@@ -59,13 +94,53 @@
         </div>
 
         <div class="outward">
-          <div class="option__block">
-            <p class="option__title">手機殼顏色</p>
-            <div class="icon"></div>
+          <div class="option__wrapper">
+            <div class="option__block" @click="isShowFrameRadio = !isShowFrameRadio">
+              <p class="option__title">手機殼顏色</p>
+              <div class="icon"></div>
+            </div>
+            <div :class="['frame__radio', { 'frame__radio--active': isShowFrameRadio }]">
+              <div
+                class="frame__radio__container"
+                v-for="(variant, index) in frames"
+                :key="variant + index"
+              >
+                <input
+                  @click="setProduct('frame', index)"
+                  :class="['frame__radio__button', selectedFrameColor.split(' ')[0]]"
+                  name="frame"
+                  type="radio"
+                />
+              </div>
+            </div>
           </div>
-          <div class="option__block">
-            <p class="option__title">背板樣式</p>
-            <div class="icon"></div>
+          <div class="option__wrapper">
+            <div class="option__block" @click="isShowBackPlateSelector = !isShowBackPlateSelector">
+              <p class="option__title">背板樣式</p>
+              <div class="icon"></div>
+            </div>
+            <div :class="['back-plate', { 'back-plate--active': isShowBackPlateSelector }]">
+              <div
+                @click="setProduct('tarnsparentBackPlate')"
+                :class="['back-plate__container', { 'back-plate__container--checked' : backPlateIndex === 0}]"
+              >
+                <img
+                  class="back-plate__image"
+                  :src="isFetched ? productsInfo[selectedDevice].tarnsparentBackPlate.variants[0].image : ''"
+                  alt="透明背板"
+                >
+              </div>
+              <div
+                @click="setProduct('backPlate')"
+                :class="['back-plate__container', { 'back-plate__container--checked' : backPlateIndex === 1}]"
+              >
+                <img
+                  class="back-plate__image"
+                  :src="isFetched ? productsInfo[selectedDevice].backPlate.variants[0].image : ''"
+                  alt="背板"
+                >
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -73,14 +148,14 @@
       <div class="checkout">
         <div class="checkout__detail">
           <p class="checkout__detail__info">產品包含:</p>
-          <p class="checkout__detail__info">手機殼外框 (黑) x 1</p>
-          <p class="checkout__detail__info">按鍵 (黑) x 3</p>
-          <p class="checkout__detail__info">背板 (透明) x 1</p>
-          <p class="checkout__detail__info">飾條 (黑) x 1</p>
+          <p class="checkout__detail__info">手機殼外框 ({{ selectedFrameColor }}) x 1</p>
+          <p class="checkout__detail__info">按鍵 ({{ selectedFrameColor }}) x 3</p>
+          <p class="checkout__detail__info">背板 ({{ selectedBackPlateColor }}) x 1</p>
+          <p class="checkout__detail__info">飾條 ({{ selectedFrameColor }}) x 1</p>
         </div>
         <div class="checkout__button__block">
           <button class="checkout__btn" @click="addToCart">加入購物車</button>
-          <p class="price">$800 TWD</p>
+          <p class="price">${{ price }} TWD</p>
         </div>
       </div>
     </div>
@@ -88,6 +163,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   components: {
   },
@@ -95,24 +172,125 @@ export default {
   },
   data () {
     return {
+      devices: ['iphone-12-pro-max', 'iphone-12-pro', 'iphone-12'],
+      productParts:
+      {
+        phone: 'type-mod-nx-phone-45degree', // 手機
+        frame: 'type-mod-nx-frame-45degree', // 外框
+        rim: 'rim', // 飾條
+        button: 'button', // 按鈕
+        backPlate: 'customized-backprint', // 背板
+        tarnsparentBackPlate: 'category-mod-nx-backplate-basic' // 透明背板
+      },
+      selectedDevice: 'iphone-12-pro-max', // 選擇的機種
+      backPlateIndex: 0, // 因為背板直接寫死，所以存選擇第幾個
+      previewMode: 'backPlate',
+      isShowFrameRadio: false,
+      isShowBackPlateSelector: false,
+      productsInfo: {}, // 處理過後的產品資料
       cartItems: [],
-      product: {}
-      // device: bind select
+      product: {}, // 目前預覽的產品
+      isFetched: false // 資料是否載完
     }
   },
   computed: {
-
+    frames () {
+      if (this.isFetched) {
+        return this.productsInfo[this.selectedDevice].frame.variants
+      } else {
+        return []
+      }
+    },
+    previewImageSrc () {
+      if (this.isFetched) {
+        return {
+          phone: this.product.phone.image,
+          frame: this.product.frame.image,
+          rim: this.product.rim.image,
+          button: this.product.button.image,
+          backPlate: this.product.backPlate.image
+        }
+      } else {
+        return {
+          phone: 'https://cdn.shopify.com/s/files/1/0740/2335/products/phone45-12-Pro_AppleGraphite_d04b461d-15c3-4e43-9b43-a828eaa4478c.png?v1603352312',
+          frame: 'https://cdn.shopify.com/s/files/1/0740/2335/products/Black-Bumper_87d2c086-7e6a-4f40-93d9-33828a249bbe.png?v=1597911746',
+          rim: 'https://cdn.shopify.com/s/files/1/0740/2335/products/Black-Rim_2e73b29c-7262-463d-b047-2f91a790068a.png?v=1597911791',
+          button: 'https://cdn.shopify.com/s/files/1/0740/2335/products/Black-Button_23534b09-c1aa-4ab4-8174-ded2bc09069e.png?v=1597911773',
+          backPlate: 'https://cdn.shopify.com/s/files/1/0740/2335/products/minisite-NPB0118526L_b4665252-c418-480f-9ca3-b84fb860ca2c.png?v=1603868182'
+        }
+      }
+    },
+    selectedFrameColor () {
+      return this.isFetched ? this.product.frame.optionsWithKey.Color : ''
+    },
+    selectedBackPlateColor () {
+      if (this.isFetched) {
+        // db.json 有Color也有color
+        return this.product.backPlate.optionsWithKey.Color ? this.product.backPlate.optionsWithKey.Color : this.product.backPlate.optionsWithKey.color
+      } else {
+        return ''
+      }
+    },
+    price () {
+      return this.isFetched ? this.product.backPlate.price : ''
+    }
+  },
+  created () {
+    this.fetchProductData()
+    this.selectedDevice = this.devices[0]
   },
   methods: {
     fetchProductData () {
-      // TODO
-      // Call API 抓取產品資料並依產品類型分類。
-      // API URL : http://localhost:3000/mod-nx
+      axios.get('http://localhost:3000/mod-nx')
+        .then(response => {
+          // 存成 {
+          //  機種A: {機殼: {...}, 按鈕: {...}...},
+          //  機種B: {機殼: {...}, 按鈕: {...}...}
+          // }
+          response.data.data.forEach(deviceInfo => {
+            this.devices.forEach(device => {
+              if (deviceInfo.tags.includes(`device-${device}`)) {
+                for (const [part, tag] of Object.entries(this.productParts)) {
+                  if (deviceInfo.tags.includes(tag)) {
+                    if (!this.productsInfo[device]) {
+                      this.productsInfo[device] = {}
+                    }
+                    this.productsInfo[device][part] = deviceInfo
+                  }
+                }
+              }
+            })
+          })
+
+          this.setProduct('init')
+          this.isFetched = true
+        })
     },
 
     addToCart () {
-      // TODO
-      // 把選取的產品加到 cartItems 裡面並 console 出來
+      this.cartItems.push(this.product)
+      console.log(this.cartItems)
+    },
+
+    setProduct (part, index) {
+      const product = Object.assign({}, this.product)
+      if (part === 'init') { // 第一次進入或切換機種
+        ['phone', 'frame', 'rim', 'button'].forEach(key => {
+          product[key] = this.productsInfo[this.selectedDevice][key].variants[0]
+        })
+        product.backPlate = this.productsInfo[this.selectedDevice].tarnsparentBackPlate.variants[0] // 預設透明背板
+      } else if (part === 'frame') { // 更換手機殼顏色
+        ['frame', 'rim', 'button'].forEach(key => {
+          product[key] = this.productsInfo[this.selectedDevice][key].variants[index]
+        })
+      } else if (part === 'tarnsparentBackPlate') { // 選擇透明背板
+        product.backPlate = this.productsInfo[this.selectedDevice].tarnsparentBackPlate.variants[0]
+        this.backPlateIndex = 0
+      } else { // 選擇普通背板
+        product.backPlate = this.productsInfo[this.selectedDevice].backPlate.variants[0]
+        this.backPlateIndex = 1
+      }
+      this.product = product
     }
   }
 }
@@ -154,6 +332,7 @@ export default {
   height: 100%
 
 .selection__area
+  z-index: 10
   display: flex
   flex-direction: column
   justify-content: space-between
@@ -206,50 +385,121 @@ export default {
   .outward
     border-top: 2px solid #D8D8D8
 
-  .option__block
-    position: relative
-    display: flex
-    justify-content: flex-start
-    align-items: center
-    padding: 20px 0
+  .option__wrapper
     border-bottom: 1px solid #D8D8D8
 
-    .option__title
-      font-size: 14px
-      font-family: 'MarkPro', 'NotoSansCJKtc-Regular'
-      color: #2D2D2D
-
-    .icon
-      position: absolute
-      right: 0
-      top: 50%
-      width: 16px
-      height: 16px
-      transform: translateY(-50%)
+    .option__block
+      position: relative
+      display: flex
+      justify-content: flex-start
+      align-items: center
+      padding: 20px 0
       cursor: pointer
 
-      &::before
-        content: ''
-        position: absolute
-        left: 0
-        width: 1px
-        height: 16px
-        background-color: #D8D8D8
-        transform: rotate(90deg)
-        transition: all .3s ease
+      .option__title
+        font-size: 14px
+        font-family: 'MarkPro', 'NotoSansCJKtc-Regular'
+        color: #2D2D2D
 
-      &::after
-        content: ''
+      .icon
         position: absolute
-        left: 0
-        width: 1px
+        right: 0
+        top: 50%
+        width: 16px
         height: 16px
-        background-color: #D8D8D8
-        transition: all .3s ease
+        transform: translateY(-50%)
 
-    p
-      margin: 0
-      padding: 0
+        &::before
+          content: ''
+          position: absolute
+          left: 0
+          width: 1px
+          height: 16px
+          background-color: #D8D8D8
+          transform: rotate(90deg)
+          transition: all .3s ease
+
+        &::after
+          content: ''
+          position: absolute
+          left: 0
+          width: 1px
+          height: 16px
+          background-color: #D8D8D8
+          transition: all .3s ease
+
+      p
+        margin: 0
+        padding: 0
+
+    .frame__radio
+      display: none
+      flex-wrap: wrap
+
+      &--active
+        display: flex
+
+      &__container
+        display: flex
+        justify-content: center
+        align-items: center
+        margin: 5px
+        padding: 2px
+        width: 15px
+        height: 15px
+        border: 1px solid #fff
+        border-radius: 50%
+
+      &__button
+        margin: 0
+        padding: 2px
+        width: 100%
+        height: 100%
+        border-radius: 50%
+        background-color: #777
+        outline: none
+        cursor: pointer
+        appearance: none
+
+        &:checked:after
+          content: ''
+          display: block
+          width: 20px
+          height: 20px
+          border: 1px solid #000
+          border-radius: 50%
+          transform: translate(-25%, -25%)
+
+        &.Mint
+          background-color: #98ff98
+        &.Navy
+          background-color: navy
+
+    .back-plate
+      display: none
+      padding: 10px 0
+
+      &--active
+        display: flex
+
+      &__container
+        display: flex
+        justify-content: center
+        align-items: center
+        box-sizing: border-box
+        margin: 0 10px
+        width: 50px
+        height: 50px
+        border-radius: 10px
+        background-color: #fff
+        cursor: pointer
+
+        &--checked
+          border: 3px solid #000
+
+      .back-plate__image
+        width: 80%
+        height: 80%
 
 .purchase__preview
   position: relative
@@ -274,10 +524,11 @@ export default {
       z-index: 1
       transition: all .3s
       transform: translate(12%,-10%)
+    &--forward
+      z-index: 2
+      transition: all .3s
     &.case
       z-index: 3
-    &.backplate
-      z-index: 2
 
 .purchase__selector-wrapper
   &.phone
@@ -292,6 +543,17 @@ export default {
   top: 50px
   right: 0
   z-index: 3
+
+  .backplate-switch__option
+    display: flex
+    justify-content: center
+    align-items: center
+    padding: 20px 0
+    cursor: pointer
+
+    .backplate-switch__option__radio,
+    .backplate-switch__mode-name
+      cursor: pointer
 
 .color-picker-title
   position: absolute
